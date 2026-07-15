ab34a3e255b213becbfedf5cd02e65dcd6c187b2
Jack Kotheimer <jkotheimer9@gmail.com>
2026-07-15 12:19:36 -0500

    Added users api to imports

--- src/server/admin/api/index.ts
+++ src/server/admin/api/index.ts
@@ -4,9 +4,11 @@
  */
 import express from 'express';
 import PostsApi from '@server/admin/api/posts';
+import UserApi from '@server/admin/api/users';
 
 const router = express.Router();
 
 router.use('/posts', PostsApi);
+router.use('/users', UserApi);
 
 export default router;
